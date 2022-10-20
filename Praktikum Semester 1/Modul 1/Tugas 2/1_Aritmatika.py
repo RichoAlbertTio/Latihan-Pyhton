60cb5db5ba74722bff34bf700e532beb714dc8d1
Richo Albert Tio <101497830+RichoAlbertTio@users.noreply.github.com>
2022-10-20 23:55:11 +0700

    Add files via upload

---
+++ Praktikum Semester 1/Modul 1/Tugas 2/1_Aritmatika.py
@@ -0,0 +1,13 @@
+# mendeklarasikan variabel serta memberikan fungsi inputan dan tipe data yang di berikan
+Bil1 = int(input("Masukan Bilangan Pertama :"))
+Bil2 = int(input("Masukan Bilangan Kedua   :"))
+# menyimpan rumus
+tambah = Bil1 + Bil2
+kurang = Bil1 - Bil2
+kali = Bil1 * Bil2
+bagi = Bil1 / Bil2
+# menampilkan hasil
+print ("Jadi hasil dari ", Bil1 , " + " , Bil2 , " = " , tambah)
+print ("                ", Bil1 , " - " , Bil2 , " = " , kurang)
+print ("                ", Bil1 , " * " , Bil2 , " = " , kali)
+print ("                ", Bil1 , " / " , Bil2 , " = " , bagi)
