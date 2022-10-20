1c336bec28193dba8d561e98f081f147def468dc
Richo Albert Tio <101497830+RichoAlbertTio@users.noreply.github.com>
2022-10-20 23:55:38 +0700

    Add files via upload

---
+++ Praktikum Semester 1/Modul 1/Tugas 2/2_PermukaanKerucut.py
@@ -0,0 +1,12 @@
+# const atau memberikan nilai tetap
+Phi = 3.14
+
+# mendeklarasikan variabel serta memberikan fungsi inputan dan tipe data yang di berikan
+r = int(input("Masukan Jari-jari kerucut :"))
+s = int(input("Masukan sisi-sisi kerucut :"))
+
+# menyimpan rumus
+Luas = Phi * r * (r * s)
+
+# menampilkan hasil
+print ("Luas permukaan kerucut :",Luas)
